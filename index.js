4e87ff7c0fcddd7e90e703f9ebb92456172e9695
koko1313 <kaloyanvellichkov@gmail.com>
2020-10-24 13:24:38 +0300

    Changing var to let

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 // hides all watched videos
-var progresses = document.getElementsByClassName("style-scope ytd-thumbnail-overlay-resume-playback-renderer");
-for(var i=0; i < progresses.length; i++) {
-	var video = progresses[i].parentElement.parentElement.parentElement.parentElement.parentElement.parentElement;
+let progresses = document.getElementsByClassName("style-scope ytd-thumbnail-overlay-resume-playback-renderer");
+for(let i=0; i < progresses.length; i++) {
+	let video = progresses[i].parentElement.parentElement.parentElement.parentElement.parentElement.parentElement;
 	video.style.display = "none";
 }
 
 // hides all videos (watched or not) that are shorter than a certain time length
-var time = 20; // time length in minutes
-var videosLength = document.getElementsByClassName("style-scope ytd-thumbnail-overlay-time-status-renderer");
-for(var i=0; i < videosLength.length; i++) {
+let time = 20; // time length in minutes
+let videosLength = document.getElementsByClassName("style-scope ytd-thumbnail-overlay-time-status-renderer");
+for(let i=0; i < videosLength.length; i++) {
 	if(videosLength[i].innerText == "") continue;
 	
 	if(videosLength[i].innerText.split(":")[0] < time) {
-		var video = videosLength[i].parentElement.parentElement.parentElement.parentElement.parentElement.parentElement;
+		let video = videosLength[i].parentElement.parentElement.parentElement.parentElement.parentElement.parentElement;
 		video.style.display = "none";
 	}
 }
