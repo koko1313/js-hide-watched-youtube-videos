227a568f475ac5b9d336dd3eb19946d75031985b
koko1313 <kaloyanvellichkov@gmail.com>
2021-01-03 01:25:26 +0200

    Logging the count of the hidden videos

--- index.js
+++ index.js
@@ -1,30 +1,60 @@
 /**
- * Hides all watched videos
+ * Hides the video.
+ * @param {Element} video the video (as a DOM element) that have to be hidden
+ * @returns {boolean} **true** if hidden, **false** if already hidden
  */
-let hideWatchedVideos = () => {
-	let progresses = document.getElementsByClassName("style-scope ytd-thumbnail-overlay-resume-playback-renderer");
-	for(let i=0; i < progresses.length; i++) {
-		let video = progresses[i].parentElement.parentElement.parentElement.parentElement.parentElement.parentElement;
+let hideVideoIfNotAlreadyHidden = (video) => {
+	if (video.style.display != "none") {
 		video.style.display = "none";
+		return true;
 	}
+	return false;
 }
 
 /**
- * Hides all videos (watched or not) that are shorter than a certain time length
+ * Hides all watched videos.
+ * Logs the count of the hidden videos.
+ * @returns {number} the count of the hidden videos
+ */
+let hideWatchedVideos = () => {
+	let countOfHiddenVideos = 0;
+	let progresses = document.getElementsByClassName("style-scope ytd-thumbnail-overlay-resume-playback-renderer");
+	for(let i=0; i < progresses.length; i++) {
+		let video = progresses[i].parentElement.parentElement.parentElement.parentElement.parentElement.parentElement;
+		if (hideVideoIfNotAlreadyHidden(video)) {
+			countOfHiddenVideos++;
+		}
+	}
+	console.log(`${countOfHiddenVideos} watched videos were hidden.`);
+	return countOfHiddenVideos;
+}
+
+/**
+ * Hides all videos (watched or not) that are shorter than a certain time length.
+ * Logs the count of the hidden videos.
  * @param {number} time time length in minutes
+ * @returns {number} the count of the hidden videos
  */
 let hideVideosShorterThan = (time) => {
+	let countOfHiddenVideos = 0;
 	let videosLength = document.getElementsByClassName("style-scope ytd-thumbnail-overlay-time-status-renderer");
 	for(let i=0; i < videosLength.length; i++) {
 		if(videosLength[i].innerText == "") continue;
 		
 		if(videosLength[i].innerText.split(":")[0] < time) {
 			let video = videosLength[i].parentElement.parentElement.parentElement.parentElement.parentElement.parentElement;
-			video.style.display = "none";
+			if (hideVideoIfNotAlreadyHidden(video)) {
+				countOfHiddenVideos++;
+			}
 		}
 	}
+	console.log(`${countOfHiddenVideos} videos shorted than ${time} were hidden.`);
+	return countOfHiddenVideos;
 }
 
+let countOfAllVideosHidden = 0;
 
-hideWatchedVideos();
-hideVideosShorterThan(20);
+countOfAllVideosHidden += hideWatchedVideos();
+countOfAllVideosHidden += hideVideosShorterThan(20);
+
+console.log(`Total videos hidden: ${countOfAllVideosHidden}`);
